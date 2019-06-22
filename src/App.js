bb85ebb51019ec37c2b4f49d916e64b0b1a39d7d
unknown <mdzeeshanali93@gmail.com>
2019-06-22 19:16:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 function App() {
+  useEffect(() => {
+    //Initialize materialize CSS
+    M.AutoInit();
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <Logs />
+        </div>
+      </Fragment>
     </div>
   );
 }
